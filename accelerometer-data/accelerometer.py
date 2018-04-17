c685052220c9786b58199749116b3ccb0603277e
JWTappert <justin@theflybook.com>
2018-04-17 09:48:27 -0700

    normalizing the data for particpants #1

--- accelerometer-data/accelerometer.py
+++ accelerometer-data/accelerometer.py
@@ -1,3 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+data = pd.read_csv('1.csv', delimiter=',', names=['id','x','y','z','label'])
+
+print data
+
+# get each column
+
+raw_x = np.array(data['x'])
+raw_y = np.array(data['y'])
+raw_z = np.array(data['z'])
+
+# normalize the data for each column
+
+norm_x = 
