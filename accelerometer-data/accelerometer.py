30b00e8efb10e27c7c7128d6620b6d46e734a58b
JWTappert <justin@theflybook.com>
2018-04-17 15:51:38 -0700

    remove 0 rows from first five csvs

--- accelerometer-data/accelerometer.py
+++ accelerometer-data/accelerometer.py
@@ -2,33 +2,40 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-data = pd.read_csv('1.csv', delimiter=',', names=['id','x','y','z','label'])
+data = pd.read_csv('3.csv', delimiter=',', names=['id','x','y','z','label'])
 
 # group the data by activity label
 grpd_data = data.groupby('label')
 
+# create index to correctly subplot the activities
+index = 1
+# dictionary to map index to activity
+act_to_index = {1:"Working at Computer", 2:"Standing up, Walking and going up/down stairs", 3:"Standing", 4:"Walking", 5:"Going up/down Stairs", 6:"Walking and Talking with someone", 7:"Talking While Standing"}
+
+plt.figure(figsize=(20,20))
 for key, item in grpd_data:
-    print key
-    x = item['x'] / np.linalg.norm(item['x'])
-    y = item['y'] / np.linalg.norm(item['y'])
-    z = item['z'] / np.linalg.norm(item['z'])
-    plt.subplot(3,1,)
-    plt.plot(x,y,z)
+    # get x, y, and z into numpy array and then normalize the data
+    xarray = np.array(item['x'])
+    x = xarray / np.linalg.norm(xarray)
+    # shift x up in order to seperate the lines
+    x = x + np.mean(x)
+    yarray = np.array(item['y'])
+    y = yarray / np.linalg.norm(yarray)
+    zarray = np.array(item['z'])
+    z = zarray / np.linalg.norm(zarray)
+    # shift z down to seperate the lines
+    z = z - np.mean(z)
     
-# normalize the data for each column
+    # plot each activity as a subplot
+    plt.subplot(7,1,index)
+    plt.plot(x, color="red")
+    plt.plot(y, color="green")
+    plt.plot(z, color="blue")
+    plt.xlabel("time (s)")
+    plt.ylabel("normalized magnitude")
+    plt.title(act_to_index[index])
+    index += 1
+    
 
-norm_x = raw_x / np.linalg.norm(raw_x)
-norm_y = raw_y / np.linalg.norm(raw_y)
-norm_z = raw_z / np.linalg.norm(raw_z)
-
-# plt.subplot(3,1,1)
-# plt.plot(norm_x)
-
-# plt.subplot(3,1,2)
-# plt.plot(norm_y)
-
-# plt.subplot(3,1,3)
-# plt.plot(norm_z)
-
-# plt.tight_layout()
-# plt.show()
+plt.tight_layout()
+plt.show()
