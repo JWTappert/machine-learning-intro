293662d3c8e999af8838c2e31b5eb3fa494cd2d4
JWTappert <tappertj@oregonstate.edu>
2018-04-17 11:45:13 -0700

    working to seperate groups of data by activity

--- accelerometer-data/accelerometer.py
+++ accelerometer-data/accelerometer.py
@@ -4,26 +4,31 @@ import pandas as pd
 
 data = pd.read_csv('1.csv', delimiter=',', names=['id','x','y','z','label'])
 
-# get each column
+# group the data by activity label
+grpd_data = data.groupby('label')
 
-raw_x = np.array(data['x'])
-raw_y = np.array(data['y'])
-raw_z = np.array(data['z'])
-
+for key, item in grpd_data:
+    print key
+    x = item['x'] / np.linalg.norm(item['x'])
+    y = item['y'] / np.linalg.norm(item['y'])
+    z = item['z'] / np.linalg.norm(item['z'])
+    plt.subplot(3,1,)
+    plt.plot(x,y,z)
+    
 # normalize the data for each column
 
 norm_x = raw_x / np.linalg.norm(raw_x)
 norm_y = raw_y / np.linalg.norm(raw_y)
 norm_z = raw_z / np.linalg.norm(raw_z)
 
-plt.subplot(3,1,1)
-plt.plot(norm_x)
+# plt.subplot(3,1,1)
+# plt.plot(norm_x)
 
-plt.subplot(3,1,2)
-plt.plot(norm_y)
+# plt.subplot(3,1,2)
+# plt.plot(norm_y)
 
-plt.subplot(3,1,3)
-plt.plot(norm_z)
+# plt.subplot(3,1,3)
+# plt.plot(norm_z)
 
-plt.tight_layout()
-plt.show()
+# plt.tight_layout()
+# plt.show()
