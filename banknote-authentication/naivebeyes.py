d031cf33e4d1c96cdbc389a7eee85c906fe57a46
JWTappert <justin@theflybook.com>
2018-04-22 15:18:59 -0700

    forgot my comments for Marc

--- banknote-authentication/naivebeyes.py
+++ banknote-authentication/naivebeyes.py
@@ -1,17 +1,22 @@
 import numpy as np
 from sklearn.naive_bayes import GaussianNB
 
+# read in training data
 train_data = np.genfromtxt("banknote_train.csv", delimiter=",")
 
+# read in the test data
 test_data = np.genfromtxt("banknote_test.csv", delimiter=",")
 
+# seperate the training data from the classes
 X = train_data[:,:-1]
+# get only the classes
 y = train_data[:,-1]
 
+# train the NB model
 gnb = GaussianNB()
-
 y_pred = gnb.fit(X, y).predict(X)
 
+# predict the test data
 test = gnb.predict(test_data)
 
 print test
--- banknote-authentication/nearestneighbor.py
+++ banknote-authentication/nearestneighbor.py
@@ -1,15 +1,21 @@
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 
+# read in training data
 train_data = np.genfromtxt("banknote_train.csv", delimiter=",")
 
+# read in test data
 test_data = np.genfromtxt("banknote_test.csv", delimiter=",")
 
+# split data points from classes in training data
 X = train_data[:,:-1]
+# get only the classes that match the data
 y = train_data[:,-1]
 
+# train the knn model
 knn = KNeighborsClassifier(n_neighbors=3)
 knn.fit(X,y)
 
+# predict the test data
 print(knn.predict(test_data))
 print(knn.predict_proba(test_data))
