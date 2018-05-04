9be1970b7338027719f2f9f6c44ddb5297b195bd
JWTappert <justin@theflybook.com>
2018-05-03 22:52:03 -0700

    adds todo

--- auto-mpg/linearRegression.py
+++ auto-mpg/linearRegression.py
@@ -4,7 +4,4 @@ import scipy.stats as stats
 
 train_data = pd.read_csv('auto-mpg-train.data', delim_whitespace=True)
 
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-    print(train_data)
-
-# test_data = np.genfromtxt('auto-mpg-test.data', delimiter=" ")
+# TODO: implement the linear regression workflow
