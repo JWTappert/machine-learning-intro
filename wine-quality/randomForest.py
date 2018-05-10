381e3861841db6a17fb8dc0c9f054d16173609e4
JWTappert <justin@theflybook.com>
2018-05-09 20:35:37 -0700

    something is definitely wrong, all type columns are 0

--- wine-quality/randomForest.py
+++ wine-quality/randomForest.py
@@ -1,46 +1,43 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import KFold
-
 
 # Get the red wine data
 red_wine_train = pd.read_csv('wine-quality-red-train.csv', delimiter=';')
-
 # Get the white wine data
 white_wine_train = pd.read_csv('wine-quality-white-train.csv', delimiter=';')
-
 # get the test data
 test_data = pd.read_csv('wine-quality-test.csv', delimiter=';')
 
+# process the data
+
 # add class column to red wine training set
 red_wine_train['class'] = 'red'
-
 # add class column to white wine training set
 white_wine_train['class'] = 'white'
-
 # concatenate the two dataframes together
 train_data = pd.concat([red_wine_train, white_wine_train])
-
 # get the features of the data for indexing
 features = train_data.columns[:12]
-
+# get the qualities
+qualities = train_data['quality']
 # get the classes and convert them to digits, i.e. 0 = red 1 = white
-y = pd.factorize(train_data['class'])[0]
+types = pd.factorize(train_data['class'])[0]
 
-# k folds cross validate to help improve our model
-#kf = KFold(n_splits=5)
-#kf.get_n_splits(train_data)
+# classify
 
 # create our random forest classifier
-clf = RandomForestClassifier()
+clf1 = RandomForestClassifier()
+# classify the type
+clf1.fit(train_data[features], types)
+type_pred = clf1.predict(test_data)
+# create new classifier
+clf2 = RandomForestClassifier()
+# classify the quality
+clf2.fit(train_data[features], qualities)
+qual_pred = clf2.predict(test_data)
 
-#for train_index, test_index in kf.split(train_data):
-#    x_train, x_test = train_data[train_index], train_data[test_index]
-#    y_train, y_test = y[train_index], y[test_index]
-#    clf.fit(x_train, y_train)
-#    print clf.predict(x_test, y_test)
+results = pd.DataFrame(columns=['id','quality','type'])
+results['quality'] = qual_pred.flatten()
+results['type'] = type_pred.tolist()
 
-clf.fit(train_data[features], y)
-print clf.predict(test_data)
+results.to_csv('tappert.csv', sep=' ')
