f8d5add06d45519b13f3870cafc686e848ab6b50
JWTappert <justin@theflybook.com>
2018-05-10 21:51:59 -0700

    Much better predictions

--- wine-quality/randomForest.py
+++ wine-quality/randomForest.py
@@ -1,8 +1,7 @@
 import pandas as pd
-from sklearn import preprocessing
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import mean_squared_error, r2_score
+# from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.pipeline import make_pipeline
 from sklearn.model_selection import GridSearchCV
 
@@ -10,78 +9,59 @@ from sklearn.model_selection import GridSearchCV
 red_wine_train = pd.read_csv('wine-quality-red-train.csv', delimiter=';')
 # Get the white wine data
 white_wine_train = pd.read_csv('wine-quality-white-train.csv', delimiter=';')
-# get the test data
-test_data = pd.read_csv('wine-quality-test.csv', delimiter=';')
-test_data = test_data.drop('ID', axis=1)
-
-# process the data
 
 # add class column to red wine training set
-#red_wine_train['class'] = 'red'
+red_with_class = red_wine_train.copy()
+red_with_class['class'] = 'red'
+
 # add class column to white wine training set
-#white_wine_train['class'] = 'white'
-# concatenate the two dataframes together
-train_data = pd.concat([red_wine_train, white_wine_train])
-# get the features of the data for indexing
-features = train_data.columns[:12]
+white_with_class = white_wine_train.copy()
+white_with_class['class'] = 'white'
+
+# concatenate our data sets together
+q_train_data = pd.concat([red_wine_train, white_wine_train])
+t_train_data = pd.concat([red_with_class, white_with_class])
+
+# get the test data
+test_data = pd.read_csv('wine-quality-test.csv', delimiter=';')
+# Get the IDs only
+IDs = test_data['ID']
+# then remove them from the dataset
+test_data = test_data.drop('ID', axis=1)
 
 # get types and feature data seperated
-#type_y = pd.factorize(train_data['class'])[0]
-#type_x = train_data[features]
+type_y = pd.factorize(t_train_data['class'])[0]
+type_x = t_train_data.drop('class', axis=1)
 
-qual_y = train_data['quality']
-qual_x = train_data.drop('quality', axis=1)
-
-
-# split the data into train and test sets to see how well our model does before actually predicting
-X_train, X_test, y_train, y_test = train_test_split(qual_x, qual_y, test_size=0.2, random_state=123, stratify=qual_y)
+# get qualities and features seperated
+qual_y = q_train_data['quality']
+qual_x = q_train_data.drop('quality', axis=1)
 
 # create the pipeline object to feed to the cross validator
-pipeline = make_pipeline(preprocessing.StandardScaler(), RandomForestClassifier(n_estimators=100))
+pipeline = make_pipeline(RandomForestClassifier(n_estimators=100))
 
 # fine tune the paramters for the CV
 hyperparameters = { 'randomforestclassifier__max_features' : ['auto', 'sqrt', 'log2'],
                   'randomforestclassifier__max_depth': [None, 5, 3, 1]}
 
 # cross validate using the pipeline
-clf = GridSearchCV(pipeline, hyperparameters, cv=10)
-
+quality_clf = GridSearchCV(pipeline, hyperparameters, cv=10)
 # train the model with subset of data
-clf.fit(X_train, y_train)
+quality_clf.fit(qual_x, qual_y)
+#predict the quality
+quality_pred = quality_clf.predict(test_data)
+# append the quality prediction to the test data so the columns match
+test_data['quality'] = quality_pred
 
-# predict on the test data
-pred = clf.predict(X_test)
-# print out scores
-print r2_score(y_test, pred)
-# quality output: 0.42666289108163247
-# type output: 0.9792664431673053
-print mean_squared_error(y_test, pred)
-# quality output: 0.43888354186718
-# type output: 0.0038498556304138597
-
-#type_pred = clf.predict(test_data)
-
-# get the qualities
-#qualities = train_data['quality']
-# get the classes and convert them to digits, i.e. 0 = red 1 = white
-
-
-# classify
-
-# create our random forest classifier
-#clf1 = RandomForestClassifier()
-# classify the type
-#clf1.fit(train_data[features], types)
-#type_pred = clf1.predict(test_data)
-# create new classifier
-#clf2 = RandomForestClassifier()
-## classify the quality
-#clf2.fit(train_data[features], qualities)
-#qual_pred = clf2.predict(test_data)
+# create new classifier for the type prediction
+type_clf = GridSearchCV(pipeline, hyperparameters, cv=10)
+# fit the data with the quality appended
+type_clf.fit(type_x, type_y)
+# predict the type
+type_pred = type_clf.predict(test_data)
 
 results = pd.DataFrame(columns=['id','quality','type'])
-#results['quality'] = qual_pred.flatten()
+results['quality'] = quality_pred.flatten()
 results['type'] = type_pred.tolist()
-#print type_pred
 
 results.to_csv('tappert.csv', sep=' ')
