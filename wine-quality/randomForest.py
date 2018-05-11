c7fd8ba79d01edecd834db5b008dcf16104a6dcc
JWTappert <justin@theflybook.com>
2018-05-10 20:42:34 -0700

    Just validating my model on the training data

--- wine-quality/randomForest.py
+++ wine-quality/randomForest.py
@@ -1,5 +1,10 @@
 import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import GridSearchCV
 
 # Get the red wine data
 red_wine_train = pd.read_csv('wine-quality-red-train.csv', delimiter=';')
@@ -18,26 +23,32 @@ white_wine_train['class'] = 'white'
 train_data = pd.concat([red_wine_train, white_wine_train])
 # get the features of the data for indexing
 features = train_data.columns[:12]
-# get the qualities
-qualities = train_data['quality']
-# get the classes and convert them to digits, i.e. 0 = red 1 = white
-types = pd.factorize(train_data['class'])[0]
 
-# classify
+# get types and feature data seperated
+type_y = pd.factorize(train_data['class'])[0]
+type_x = train_data[features]
 
-# create our random forest classifier
-clf1 = RandomForestClassifier()
-# classify the type
-clf1.fit(train_data[features], types)
-type_pred = clf1.predict(test_data)
-# create new classifier
-clf2 = RandomForestClassifier()
-# classify the quality
-clf2.fit(train_data[features], qualities)
-qual_pred = clf2.predict(test_data)
+# split the data into train and test sets to see how well our model does before actually predicting
+X_train, X_test, y_train, y_test = train_test_split(type_x, type_y, test_size=0.2, random_state=123, stratify=type_y)
 
-results = pd.DataFrame(columns=['id','quality','type'])
-results['quality'] = qual_pred.flatten()
-results['type'] = type_pred.tolist()
+# create the pipeline object to feed to the cross validator
+pipeline = make_pipeline(RandomForestClassifier(n_estimators=100))
 
-results.to_csv('tappert.csv', sep=' ')
+# fine tune the paramters for the CV
+hyperparameters = { 'randomforestclassifier__max_features' : ['auto', 'sqrt', 'log2'],
+                  'randomforestclassifier__max_depth': [None, 5, 3, 1]}
+
+# cross validate using the pipeline
+clf = GridSearchCV(pipeline, hyperparameters, cv=10)
+
+# train the model with subset of data
+clf.fit(X_train, y_train)
+
+# predict on the test data
+pred = clf.predict(X_test)
+# print out scores
+print r2_score(y_test, pred)
+# out: 0.9792664431673053
+print mean_squared_error(y_test, pred)
+# out: 0.0038498556304138597
+
