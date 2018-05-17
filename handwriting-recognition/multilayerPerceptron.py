a76c69dbe003912ba72f72ffeb2a1eb0f44373d5
JWTappert <justin@theflybook.com>
2018-05-16 20:35:13 -0700

    TUNING THIS BITCH UP. CPU AT 9000

--- handwriting-recognition/multilayerPerceptron.py
+++ handwriting-recognition/multilayerPerceptron.py
@@ -19,10 +19,27 @@ y_train = raw_data.iloc[:, 256:]
 
 pipeline = make_pipeline(MLPClassifier())
 
+hyperparameters = { 'mlpclassifier__hidden_layer_sizes': [(1,), (10,), (20,), (40,), (80,)],
+                    'mlpclassifier__activation': ['identity', 'logistic', 'tanh', 'relu'],
+                    'mlpclassifier__solver': ['lbfgs', 'sgd', 'adam'],
+                    'mlpclassifier__learning_rate': ['constant', 'invscaling', 'adaptive'],
+                    'mlpclassifier__random_state': [1, 1000, 100000, 1000000, 123456789]}
+
 # cross validate using the pipeline
-clf = GridSearchCV(pipeline, cv=10)
+clf = GridSearchCV(pipeline, hyperparameters, cv=10)
 # train the model with subset of data
 clf.fit(x_train, y_train)
 
 # get the prediction
 pred = clf.predict(test_data)
+
+results = pd.DataFrame(columns=['id', 'prediction'])
+
+# iterate over the columns
+for i in pred.index:
+    # iterate over the rows
+    for j in pred.columns:
+        results['id'] = i
+        if pred[i,j] == 1:
+            results['prediction'] = j
+        
