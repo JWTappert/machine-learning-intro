5387a43a9b05ddd553ea8631c6ba26fb1b193040
JWTappert <justin@theflybook.com>
2018-06-06 21:09:47 -0700

    adding some shit

--- final/cleanup.py
+++ final/cleanup.py
@@ -1,8 +1,16 @@
 import numpy as np
 import pandas as pd
 
+# read in the data
 data = pd.read_csv('flybook-data.csv')
 
-data2 = data.dropna()
+# drop all rows where there is a NaN
+data = data.dropna()
 
-data2.to_csv('raw-data.csv')
+# drop all rows that have a 0 value for total revenue
+data = data[data.TotalRev != 0]
+
+
+
+
+data.to_csv('raw-data.csv')
